a245989ed9f23046759423eee64f16d0690ae784
Michael-J-Sanofi <139016292+Michael-J-Sanofi@users.noreply.github.com>
2024-11-08 05:13:54 +0000

    followed the lab

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 // all code for this lab to be completed in this file
+currenTemperature = 32
+console.log("Current Temperature:", currenTemperature)
+
+//two possibilites: it's freezing or it's not
+
+if (currenTemperature <= 32) {
+    console.log('It is freezing.')
+} else {
+    console.log('It is not freezing.')
+}
+
+let myAge = 52
+console.log("My Age:", myAge)
+
+let yourAge = 39
+console.log("Your Age:", yourAge)
+
+if  (myAge === yourAge) {
+    console.log('We are the same age.')
+} else if (myAge > yourAge) {
+    console.log('I am older than you.')
+} else if (myAge < yourAge)
+    console.log('I am younger than you.')
+ else {
+    console.log("Error! Try again")
+}
+
